5f68edbb4155a9723b9b8cb46ebaa5e198456f8c
levtienbk56 <levtienbk56@gmail.com>
2017-04-09 19:16:39 +0900

    update data

--- src/main/java/test/DataGenerator.java
+++ src/main/java/test/DataGenerator.java
@@ -11,6 +11,7 @@ public class DataGenerator {
 	public static void main(String[] args) throws IOException {
 		String content = "";
 		int count = 0;
+		long base = 30;
 		long extra = 0;
 		long extra2 = 0;
 		long extra3 = 0;
@@ -19,10 +20,17 @@ public class DataGenerator {
 			extra += Math.round(Math.random() * 20);
 			extra2 = 0;
 			for (int m = 0; m < 12; m++) {
-				extra2 = Math.round(Math.random() * 10);
+				extra2 = Math.round(Math.random() * 20);
 				for (int d = 1; d < 29; d++) {
-					extra3 = Math.round(Math.random() * 100);
-					long x = Math.round(extra - extra2 + 30 + extra3);
+					int a = 30;
+					if (a % 3 == 0) {
+						a = 15;
+					}
+					if (a % 4 == 0) {
+						a = 100;
+					}
+					extra3 = Math.round(Math.random() * a);
+					long x = Math.round(extra - extra2 + base + extra3);
 					String str = "[new Date(" + y + "," + m + "," + d + "), " + x + "],\n";
 					content += str;
 					count++;
